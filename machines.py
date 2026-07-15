7719f86a7514c29f99eb4ab49336de17bf3c332b
grrayya <grrayya11@gmail.com>
2026-07-14 22:53:14 -0700

    Update machines.py

--- machines.py
+++ machines.py
@@ -2,8 +2,8 @@ class BrewingUnit:
     def __init__(self):
         self.water = 1000
 
-    def brew(self):
+    def brew_espresso(self):
         if self.water < 30:
-            raise RuntimeError("Water tank empty")
+            raise RuntimeError("Refill water tank before brewing")
         self.water -= 30
         return "Espresso Shot"
