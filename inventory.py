c3977832ddf225b99515f5373202f2840d504cb4
grrayya <grrayya11@gmail.com>
2026-07-14 22:53:22 -0700

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -3,9 +3,16 @@ class CafeInventory:
         self.stock = {'Kalo Jam': 12, 'Kachagolla': 20, 'Sandesh': 15, 'Mishti Doi': 8}
         self.ingredients = {'beans': 500, 'milk': 2000}
 
-    def use_ingredients(self, beans_needed, milk_needed):
-        if self.ingredients['beans'] < beans_needed or self.ingredients['milk'] < milk_needed:
+    def consume_coffee_supplies(self):
+        if self.ingredients['beans'] < 20 or self.ingredients['milk'] < 50:
             return False
-        self.ingredients['beans'] -= beans_needed
-        self.ingredients['milk'] -= milk_needed
+        
+        self.ingredients['beans'] -= 20
+        self.ingredients['milk'] -= 50
         return True
+
+    def reduce_sweet_stock(self, sweet_name):
+        if self.stock.get(sweet_name, 0) > 0:
+            self.stock[sweet_name] -= 1
+            return True
+        return False
